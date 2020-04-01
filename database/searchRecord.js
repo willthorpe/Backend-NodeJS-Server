e833105977dffd299c3213a60e5e004620370ada
Will <willjthorpe@gmail.com>
2020-04-01 18:57:24 +0100

    change method of calculating scores in search

--- database/searchRecord.js
+++ database/searchRecord.js
@@ -13,22 +13,28 @@ function searchRecipe(userIngredients, recipes, searchParameters, diets, allergi
     for (var i = 0; i < searchParameters.length; i++) {
         preferenceTotal = preferenceTotal + searchParameters[i]
     }
-    for (var j = 0; j < searchParameters.length; j++) {
-        searchParameters[j] = searchParameters[j] / preferenceTotal * 100
+    if(preferenceTotal !== 0){
+        for (var j = 0; j < searchParameters.length; j++) {
+            searchParameters[j] = parseFloat(searchParameters[j]) / parseFloat(preferenceTotal)
+        }
     }
 
+    console.log(searchParameters);
     /**
      * Algorithm Section 1 - Remove recipes with ingredients not matching allergies
      */
     for (var k = 0; k < recipes[0].data.length; k++) {
+        console.log(recipes[0].data[0].row[0].name);
         kMeansData.push([]);
-        var score = 0;
+        var recipeScore = [];
+        for (var preference = 0; preference < searchParameters.length; preference++) {
+            recipeScore.push(0);
+        }
         var totalRecipeWeight = 0;
         var meetsAllergies = true;
         for (var l = 0; l < recipes[0].data[k].row[1].length; l++) {
             //Exclude ingredients that don't meet allergy concerns
             var ingredient = recipes[0].data[k].row[1][l];
-            console.log(ingredient);
             var ingredientAllergies = ingredient[0].healthLabels.split(",");
             for (var a = 0; a < allergies.length; a++) {
                 if (allergies[a].value === 1 && !ingredientAllergies.includes(allergies[a].name)) {
@@ -47,18 +53,15 @@ function searchRecipe(userIngredients, recipes, searchParameters, diets, allergi
          */
         for (var m = 0; m < recipes[0].data[k].row[1].length; m++) {
             //Check Prefer Lighter Weight - 1
-            console.log("Lighter Weight");
             var ingredient = recipes[0].data[k].row[1][m];
             if (!kMeansLabels.includes(ingredient[0].name)) {
                 kMeansLabels.push(ingredient[0].name);
             }
             kMeansData[k][kMeansLabels.indexOf(ingredient[0].name)] = ingredient[1].weight;
             if (ingredient[1].weight <= 300) {
-                score = score + (searchParameters[1] * ((ingredient[1].weight) / totalRecipeWeight));
-                console.log(score);
+                recipeScore[1] += ingredient[1].weight / totalRecipeWeight;
             }
             //Check Prefer Matching Diet - 7
-            console.log("Prefer Matching Diet");
             var matchDiets = true;
             var ingredientDiets = ingredient[0].dietLabels.split(",");
             for (var d = 0; d < diets.length; d++) {
@@ -67,61 +70,53 @@ function searchRecipe(userIngredients, recipes, searchParameters, diets, allergi
                 }
             }
             if (matchDiets === true) {
-                score = score + (searchParameters[7] * (ingredient[1].weight / totalRecipeWeight));
-                console.log(score)
+                recipeScore[7] += ingredient[1].weight / totalRecipeWeight;
             }
             var varietyIngredient = true;
             for (var n = 0; n < userIngredients.data.length; n++) {
                 var userIngredient = userIngredients.data[n].row[0];
                 //Check Prefer Owned Ingredients - 0
-                console.log("Prefer Owned Ingredients");
                 if (ingredient[0].name === userIngredient.name && userIngredient.amount >= ingredient[1].amount) {
-                    console.log("Owned Ingredient");
-                    score = score + (searchParameters[0] * (ingredient[1].weight / totalRecipeWeight));
-                    console.log(score);
+                    recipeScore[0] += ingredient[1].weight / totalRecipeWeight;
                 } else if (ingredient[0].name === userIngredient.name)
                 //Check Prefer Variety - 2
                     varietyIngredient = false;
             }
-            console.log("Variety Ingredient");
             if (varietyIngredient === true) {
-                score = score + (searchParameters[2] * (ingredient[1].weight / totalRecipeWeight));
-                console.log(score);
+                recipeScore[2] += ingredient[1].weight / totalRecipeWeight;
             }
         }
 
         //Check Popular Recipes - 3
-        console.log("Popular Recipes");
         var popularity = recipes[1].data[0].row[0];
         if (popularity <= 20) {
-            score = score + (popularity / 20 * searchParameters[3]);
-            console.log(score);
+            recipeScore[3] += popularity / 20;
         }
 
         //Check Prefer Less Ingredients - 4
-        console.log("Less ingredients");
         var ingredients = recipes[0].data[k].row[1].length;
         if (ingredients <= 10) {
-            score = score + (ingredients / 10 * searchParameters[4]);
-            console.log(score);
+            recipeScore[4] += ingredients / 10;
         }
 
         //Check Prefer Less Complex Recipes - 5
-        console.log("Less complex recipes");
         var method = JSON.parse(recipes[0].data[k].row[0].method).length;
         if (method <= 10) {
-            score = score + (method / 10 * searchParameters[5]);
-            console.log(score);
+            recipeScore[5] += method / 10;
         }
 
         //Check Prefer Shorter Recipes - 6
-        console.log("Shorter Recipes");
         var time = parseInt(recipes[0].data[k].row[0].cookTime) + parseInt(recipes[0].data[k].row[0].prepTime)
         if (time <= 60) {
-            score = score + (time / 60 * searchParameters[6]);
-            console.log(score);
+            recipeScore[6] += time / 60;
         }
 
+        //Work out final score
+        for(var parameter = 0; parameter < recipeScore.length; parameter++){
+            recipeScore[parameter] *= 100 * searchParameters[parameter]
+        }
+
+        //Add to score array
         var recipeDetails = recipes[0].data[k].row[0];
         var methodList = JSON.parse(recipes[0].data[k].row[0].method);
         var ingredientList = recipes[0].data[k].row[1];
@@ -129,10 +124,10 @@ function searchRecipe(userIngredients, recipes, searchParameters, diets, allergi
             recipe: recipeDetails,
             method: methodList,
             ingredients: ingredientList,
-            score: Math.round(score)
+            score: Math.round(recipeScore.reduce((a,b) => a+b, 0))
         });
     }
-
+    console.log("finished");
     //Sort recipes by score descending:
     recipeScores.sort(function (a, b) {
         return b.score - a.score
@@ -188,7 +183,7 @@ function searchRecipe(userIngredients, recipes, searchParameters, diets, allergi
     //Find duplicate clusters
     var duplicates = [];
     for (var u = 0; u < clusters.centroids.length; u++) {
-        if (clusters.centroids[u].size > 1){
+        if (clusters.centroids[u].size > 1) {
             duplicates.push(u);
         }
     }
@@ -196,13 +191,13 @@ function searchRecipe(userIngredients, recipes, searchParameters, diets, allergi
     //Remove duplicate elements
     if (duplicates.length > 0) {
         for (var v = clusters.clusters.length - 1; v >= 0; v--) {
-            if(duplicates.includes(clusters.clusters[v]) && clusters.clusters[v] > -1){
-                var count = clusters.clusters.reduce(function(n, val) {
+            if (duplicates.includes(clusters.clusters[v]) && clusters.clusters[v] > -1) {
+                var count = clusters.clusters.reduce(function (n, val) {
                     return n + (val === clusters.clusters[v]);
                 }, 0);
-                if (count > 1){
+                if (count > 1) {
                     //Remove recipe from array
-                    recipeScores.splice(v,1);
+                    recipeScores.splice(v, 1);
                     clusters.clusters[v] = -1;
                 }
                 console.log(clusters);
