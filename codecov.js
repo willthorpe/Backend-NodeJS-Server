858f9032c1646e5f8c046a898c32e5812c4fb185
Will <willjthorpe@gmail.com>
2020-04-02 21:09:13 +0100

    fix codecov token

--- codecov.js
+++ codecov.js
@@ -1,3 +1,3 @@
 const shell = require('shelljs');
 
-shell.exec(' bash <(curl -s https://codecov.io/bash) -t' + process.env.CODECOV_TOKEN);
+shell.exec(' bash <(curl -s https://codecov.io/bash) -t ' + process.env.CODECOV_TOKEN);
