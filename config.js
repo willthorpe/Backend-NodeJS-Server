d92924ea0d3767c31b9f52f307a5de115bb4ea8b
Will <willjthorpe@gmail.com>
2020-04-02 19:29:09 +0100

    hopefully have travis working

--- config.js
+++ config.js
@@ -1,10 +1,10 @@
 var config = {};
 
-config.url = "";
-config.edamam_url = "";
-config.edamam_app_key = "";
-config.edamam_app_id = "";
-config.tesco_primary_key = "";
-config.spoonacular_url = "";
-config.spoonacular_key = "";
+config.url = process.env.URL;
+config.edamam_url = process.env.EDAMAM_URL;
+config.edamam_app_key = process.env.EDAMAM_APP_KEY;
+config.edamam_app_id = process.env.EDAMAM_APP_ID;
+config.tesco_primary_key = process.env.TESCO_PRIMARY_KEY;
+config.spoonacular_url = process.env.SPOONACULAR_URL;
+config.spoonacular_key = process.env.SPOONACULAR_KEY;
 module.exports = config;
