f9a7cf5188eab46f49d183c6e3e41b63da0bb280
Will <willjthorpe@gmail.com>
2020-04-02 19:20:37 +0100

    changes to travis

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+console.log(process.env);
 const create = require("./database/createRecord");
 const fetch = require("./database/fetchRecord");
 const update = require("./database/updateRecord");
--- config.js
+++ config.js
@@ -1,2 +1,10 @@
 var config = {};
+
+config.url = "";
+config.edamam_url = "";
+config.edamam_app_key = "";
+config.edamam_app_id = "";
+config.tesco_primary_key = "";
+config.spoonacular_url = "";
+config.spoonacular_key = "";
 module.exports = config;
