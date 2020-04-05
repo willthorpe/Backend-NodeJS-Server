a0220f8d7dfa750b7c718fcf81bf82502c0a3b13
Will <willjthorpe@gmail.com>
2020-04-05 18:11:52 +0100

    test formatting of spoonacular data

--- tests/spoonacular.test.js
+++ tests/spoonacular.test.js
@@ -5,3 +5,16 @@ test('spoonacular pull 1 main course', async () => {
     expect(response.data.recipes.length).toBe(1);
     expect(response.data.recipes[0]['dishTypes']).toContain("main course");
 });
+
+test('spoonacular format test', async () => {
+    var recipeResponse = await spoonacular.pullRecipes(1);
+    var recipeList = recipeResponse.data.recipes;
+    var response = await spoonacular.formatRecipes(1, recipeList);
+    expect(response[0]['name'] = recipeList[0]['title']);
+    expect(response[0]['servings'] = recipeList[0]['servings']);
+    expect(response[0]['prepTime'] = recipeList[0]['preparationMinutes']);
+    expect(response[0]['cookTime'] = recipeList[0]['cookingMinutes']);
+    expect(response[0]['user'] = 'admin');
+    expect(response[0]['ingredients'][0]['name'] = recipeList[0]['extendedIngredients'][0]['name']);
+    expect(response[0]['methods'][0] = recipeList[0]['analyzedInstructions'][0]['steps'][0]['step']);
+});
